3d33d80ddc7718b8efb8241cb08f1a1ad173905c
Dylan Lu <dylanelu@gmail.com>
2023-07-23 09:46:54 -0700

    updates from vacation

--- src/routes/app/leaderboard/+page.svelte
+++ src/routes/app/leaderboard/+page.svelte
@@ -1,26 +1,80 @@
-<script>
+<script lang="ts">
 	import Container from '$lib/components/Container.svelte';
+
+	let showLeaderboard = true;
+
+	let tmpData = [
+		{
+			rank: 1,
+			name: 'Dylan Lu',
+			kills: 18
+		},
+		{
+			rank: 2,
+			name: 'Ethan Fu',
+			kills: 14
+		},
+		{
+			rank: 3,
+			name: 'James Ford',
+			kills: 12
+		},
+		{
+			rank: 4,
+			name: 'Hayden Chong',
+			kills: 6
+		},
+		{
+			rank: 5,
+			name: 'Ethan Chen',
+			kills: 6
+		}
+	];
 </script>
 
 <Container class="flex h-full w-screen flex-col space-y-4 pt-8">
 	<div class="rounded-lg bg-neutral-800 p-8">
 		<div class="flex">
-			<button class="text-md w-full rounded-xl bg-neutral-700 px-4 py-2">Leaderboard</button>
-			<button class="text-md w-full rounded-xl px-4 py-2">Kill Feed</button>
+			<button
+				class={`text-md w-full rounded-xl ${showLeaderboard ? 'bg-neutral-700' : ''} px-4 py-2`}
+				on:click={() => (showLeaderboard = true)}>Leaderboard</button
+			>
+			<button
+				class={`text-md w-full rounded-xl ${!showLeaderboard ? 'bg-neutral-700' : ''} px-4 py-2`}
+				on:click={() => (showLeaderboard = false)}>Kill Feed</button
+			>
 		</div>
-		<div class="mt-4 flex space-x-8">
-			<div class="flex flex-col items-center justify-center">
-				<div class="text-neutral-400">Rank</div>
-				<div class="text-2xl">1</div>
+		{#if showLeaderboard}
+			<div class="mt-4 flex px-4 py-2 text-neutral-400">
+				<div class="w-16 text-left">Rank</div>
+				<div class="flex-1">Name</div>
+				<div class="w-16 text-right">Kills</div>
 			</div>
-			<div class="flex flex-1 flex-col items-start justify-center overflow-hidden">
-				<div class="text-neutral-400">Name</div>
-				<div class="whitespace-nowrap text-2xl">Dylan Lu</div>
+			<div class="flex flex-col space-y-2">
+				{#each [1, 2, 3, 4, 5, 6, 7] as num}
+					<a href="/app/profile/@me" class="flex rounded-lg bg-neutral-700 px-4 py-2">
+						<div class="w-16 pl-4 text-left">1</div>
+						<div class="flex-1 font-bold">Dylan Lu</div>
+						<div class="w-16 pr-2 text-right">13</div>
+					</a>
+				{/each}
 			</div>
-			<div class="flex flex-col items-center justify-center">
-				<div class="text-neutral-400">Kills</div>
-				<div class="text-2xl">18</div>
+		{:else}
+			<div class="flex flex-col space-y-2">
+				{#each [1, 2, 3, 4, 5, 6, 7] as num}
+					<div class="mt-4 flex flex-col rounded-lg bg-neutral-700 px-4 py-2">
+						<div>
+							<a href="/app/profile/@me" class="font-bold">Dylan Lu</a>{' '}<span
+								class="text-neutral-300">eliminated</span
+							>
+						</div>
+						<div class="flex justify-between">
+							<a href="/app/profile/@me" class="font-bold">James Ford</a>
+							<div class="text-sm text-neutral-300">2 hours ago</div>
+						</div>
+					</div>
+				{/each}
 			</div>
-		</div>
+		{/if}
 	</div>
 </Container>
