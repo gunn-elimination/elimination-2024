a593afb524b2d99f9965acb2f3b67798358c78ba
Dylan Lu <dylanelu@gmail.com>
2023-08-25 21:31:49 -0700

    don't remember what I did here, but I updated supabase

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -12,27 +12,27 @@
 <Container class="flex h-full w-screen flex-col space-y-4 pt-8">
 	<!-- Rule for Today -->
 	<div class="flex flex-col items-start rounded-lg bg-neutral-800 p-6">
-		<h2 class="text-lg text-neutral-400">Today's Challenge</h2>
-		<p class="text-lg text-neutral-300">{challenge}</p>
+		<h2 class="text-sm text-neutral-400">Today's Challenge</h2>
+		<p class="text-2xl text-neutral-300">{challenge}</p>
 	</div>
 	<!-- Target -->
 	<div class="flex rounded-lg bg-red-900/20 p-6">
 		<div class="flex-1">
-			<h2 class="text-lg text-neutral-400">Target</h2>
+			<h2 class="text-sm text-neutral-400">Target</h2>
 			<p class="text-2xl font-bold text-red-500">{target}</p>
 		</div>
 		<div class="mr-4 text-center">
-			<h2 class="text-lg text-neutral-400">Rank</h2>
+			<h2 class="text-sm text-neutral-400">Rank</h2>
 			<p class="text-2xl font-bold text-red-500">{rank}</p>
 		</div>
 		<div class="text-center">
-			<h2 class="text-lg text-neutral-400">Kills</h2>
+			<h2 class="text-sm text-neutral-400">Kills</h2>
 			<p class="text-2xl font-bold text-red-500">{kills}</p>
 		</div>
 	</div>
 	<!-- Controls-->
 	<div class="class flex-col rounded-lg bg-neutral-800 p-6">
-		<h2 class="mb-4 text-lg text-neutral-400">Controls</h2>
+		<h2 class="mb-4 text-sm text-neutral-400">Controls</h2>
 		<div class="flex flex-col space-y-2">
 			<button class="mb-4 rounded-lg bg-neutral-700 px-4 py-2 text-lg font-bold"
 				>Show Your Code</button
--- src/routes/app/activity/+page.svelte
+++ src/routes/app/activity/+page.svelte
@@ -36,30 +36,27 @@
 	<div class="rounded-lg bg-neutral-800 p-8">
 		<div class="flex">
 			<button
-				class={`text-md w-full rounded-xl font-semibold ${
-					showLeaderboard ? 'bg-neutral-700' : ''
+				class={`text-md w-full rounded-lg ${
+					showLeaderboard ? 'bg-neutral-600 text-neutral-200' : 'text-neutral-400'
 				} px-4 py-2`}
 				on:click={() => (showLeaderboard = true)}>Leaderboard</button
 			>
 			<button
-				class={`text-md w-full rounded-xl font-semibold ${
-					!showLeaderboard ? 'bg-neutral-700' : ''
+				class={`text-md w-full rounded-lg ${
+					!showLeaderboard ? 'bg-neutral-600 text-neutral-200' : 'text-neutral-400'
 				} px-4 py-2`}
 				on:click={() => (showLeaderboard = false)}>Kill Feed</button
 			>
 		</div>
 		{#if showLeaderboard}
 			<div class="mt-4 flex px-4 py-2 text-neutral-400">
-				<div class="w-16 text-left text-neutral-300">Rank</div>
-				<div class="flex-1 text-neutral-300">Name</div>
-				<div class="w-16 text-right text-neutral-300">Kills</div>
+				<div class="w-16 text-left text-sm text-neutral-300">Rank</div>
+				<div class="flex-1 text-sm text-neutral-300">Name</div>
+				<div class="w-16 text-right text-sm text-neutral-300">Kills</div>
 			</div>
-			<div class="flex flex-col space-y-2">
+			<div class="flex flex-col space-y-3">
 				{#each [1, 2, 3, 4, 5, 6, 7] as num}
-					<a
-						href="/app/profile/@me"
-						class={`flex rounded-lg bg-neutral-700/70 ${num <= 3 ? 'py-4' : 'py-2'} px-4`}
-					>
+					<a href="/app/profile/@me" class={`flex rounded-lg bg-neutral-700/70 px-4 py-2`}>
 						<div class="w-16 pl-4 text-left text-neutral-200">{num}</div>
 						<div class="flex-1 font-semibold text-neutral-200">Dylan Lu</div>
 						<div class="w-16 pr-2 text-right text-neutral-200">13</div>
@@ -67,7 +64,7 @@
 				{/each}
 			</div>
 		{:else}
-			<div class="flex flex-col space-y-2">
+			<div class="flex flex-col space-y-3">
 				{#each [1, 2, 3, 4, 5, 6, 7] as num}
 					<div class="mt-4 flex flex-col rounded-lg bg-neutral-700/70 px-4 py-2">
 						<div>
