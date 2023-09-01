f18f32303aaecd217e034d58a9c66c67047e6308
Dylan Lu <dylanelu@gmail.com>
2023-08-31 22:32:26 -0700

    idk

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -18,29 +18,32 @@
 		<div class="flex">
 			<div class="flex-1 flex-col">
 				<div class="text-sm text-neutral-400">95030486</div>
-				<div class="text-3xl text-white">Ethan Fu</div>
+				<div class="text-3xl text-white">Dylan Lu</div>
 			</div>
 			<div class="mr-4 text-right">
 				<div class=" text-sm text-neutral-400">RANK</div>
-				<div class="text-3xl text-white">12</div>
+				<div class="text-3xl text-white">1</div>
 			</div>
 			<div class="text-right">
 				<div class="text-sm text-neutral-400">KILLS</div>
-				<div class="text-3xl text-white">7</div>
+				<div class="text-3xl text-white">18</div>
 			</div>
 		</div>
-		<div class="mt-6 rounded-lg bg-neutral-700 px-8 py-6 shadow-xl">
-			<div class="text-sm text-neutral-500">TARGET</div>
-			<div class="mt-3 flex w-full justify-center space-x-2">
-				<button class="rounded-lg bg-red-600 px-6 py-2 text-sm font-bold text-red-900 shadow-md"
-					>ELIMINATE</button
-				>
-				<button class="rounded-lg px-6 py-2 text-sm text-gray-400">REPORT</button>
-			</div>
-		</div>
-		<div class="mt-4 rounded-lg bg-neutral-700 px-8 py-6 shadow-xl">
-			<div class="text-sm text-neutral-500">TODAY</div>
+
+		<div class="mt-8 rounded-lg bg-neutral-700 px-8 py-6 shadow-xl">
+			<div class="text-sm text-neutral-400">TODAY</div>
 			<div class="text-lg">Wear a hat to stay safe</div>
 		</div>
 	</div>
 </div>
+
+<!-- Target Card -->
+<!-- <div class="mt-6 rounded-lg bg-neutral-700 px-8 py-6 shadow-xl">
+	<div class="text-sm text-neutral-400">TARGET</div>
+	<div class="mt-3 flex w-full justify-center space-x-2">
+		<button class="rounded-lg bg-red-600 px-6 py-2 text-sm font-bold text-red-900 shadow-md"
+			>ELIMINATE</button
+		>
+		<button class="rounded-lg px-6 py-2 text-sm text-gray-300">REPORT</button>
+	</div>
+</div> -->
