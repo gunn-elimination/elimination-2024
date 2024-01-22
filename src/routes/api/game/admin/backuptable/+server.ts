51fc31019be35c10f039b5ab2469cfc564302eb9
Dylan Lu <dylanelu@gmail.com>
2024-01-22 15:22:56 -0800

    custom table backup

---
+++ src/routes/api/game/admin/backuptable/+server.ts
@@ -0,0 +1,33 @@
+import { error } from '@sveltejs/kit';
+import JSZip from 'jszip';
+import { format } from 'date-fns';
+
+export const GET = async ({ locals: { supabaseAdmin, getRole } }) => {
+	const role = await getRole();
+	if (role !== 'Admin') {
+		throw error(403, { message: 'Unauthorized' });
+	}
+
+	const tables = ['players', 'kill_feed', 'targets'];
+	const zip = new JSZip();
+	const folderName = format(new Date(), 'yyyy-MM-dd');
+	const folder = zip.folder(folderName);
+
+	if (!folder) throw error(500, 'Error creating folder');
+
+	for (const table of tables) {
+		const { data, error: tableError } = await supabaseAdmin.from(table).select('*').csv();
+		if (tableError) throw error(500, `Error fetching ${table}`);
+		folder.file(`${table}.csv`, data);
+	}
+
+	const zipBlob = await zip.generateAsync({ type: 'blob' });
+
+	return new Response(zipBlob, {
+		status: 200,
+		headers: {
+			'Content-Disposition': `attachment; filename="${folderName}.zip"`,
+			'Content-Type': 'application/zip'
+		}
+	});
+};
--- src/routes/app/admin/+page.svelte
+++ src/routes/app/admin/+page.svelte
@@ -58,6 +58,28 @@
 			})
 			.catch((err) => (manageError = err));
 	};
+
+	const exportTables = async () => {
+		// Download zip (based on ChatGPT)
+		try {
+			const response = await fetch($page.url.origin + '/api/game/admin/backuptable');
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+
+			const blob = await response.blob();
+			const url = window.URL.createObjectURL(blob);
+			const a = document.createElement('a');
+			a.href = url;
+			a.download = 'tables.zip';
+			document.body.appendChild(a);
+			a.click();
+			window.URL.revokeObjectURL(url);
+			document.body.removeChild(a);
+		} catch (error) {
+			console.error('Error:', error);
+		}
+	};
 </script>
 
 <Container class="">
@@ -69,7 +91,7 @@
 			on:click={resetGame}>RESET GAME</button
 		>
 		<button
-			class="ml-2 w-full rounded-lg bg-black px-4 py-2 font-bold md:w-auto"
+			class="ml-2 w-full rounded-lg bg-neutral-600 px-4 py-2 font-bold md:w-auto"
 			on:click={shuffleTargets}>SHUFFLE TARGETS</button
 		>
 		<div class="text-lg text-green-500">{dangerZoneMessage}</div>
@@ -94,5 +116,11 @@
 			<div class="text-lg text-green-500">{manageMessage}</div>
 			<div class="text-lg text-red-500">{manageError}</div>
 		</div>
+	</div>
+	<div class="mt-4 rounded-lg bg-neutral-700 p-4">
+		<h1 class="mb-4 text-xl font-semibold">Database Backups</h1>
+		<button class="rounded-lg bg-neutral-600 px-4 py-2 font-bold text-white" on:click={exportTables}
+			>DOWNLOAD CSV</button
+		>
 	</div>
 </Container>
