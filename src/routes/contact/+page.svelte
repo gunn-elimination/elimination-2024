ef78b2ce4dd75a8a6ddd69779253fb0db3f6e0e9
ethzhou <ethzhou@gmail.com>
2024-02-02 18:47:51 -0800

    contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,13 +4,44 @@
 </script>
 
 <Container class="">
-	<h1 class="mt-16 text-center text-4xl lg:text-5xl">
-		<div>Contact</div>
-	</h1>
-	<div class="mt-4 flex flex-col space-y-2 rounded-lg p-4 text-center text-neutral-300">
-		<div class="rounded-lg bg-neutral-900 px-4 py-2">Ruth Jaquette: rj30473@pausd.us</div>
-		<div class="rounded-lg bg-neutral-900 px-4 py-2">Dylan Lu: dl30486@pausd.us</div>
-		<div class="rounded-lg bg-neutral-900 px-4 py-2">Mrs. Hall: lhall@pausd.org</div>
+	<h1 class="-z-10 mb-6 mt-12 font-advent text-9xl text-zinc-700 max-md:text-7xl">contact</h1>
+	<div class="px-4">
+		<div class="font-raleway text-lg leading-5 text-gray-500">
+			For reporting, please CC the other person in your email. Remember to read all rules before
+			sending us anything.
+		</div>
+		<div class="flex flex-col gap-2 rounded-lg font-dosis text-gray-400">
+			<div
+				class="relative px-16 py-6 before:absolute before:-left-40 before:top-9 before:h-4 before:w-32 before:border-l-[1px] before:border-t-[1px] before:border-gray-600 after:absolute after:-right-10 after:-top-4 after:h-[36rem] after:w-5 after:border-r-[1px] after:border-t-[1px] after:border-zinc-600 max-sm:px-0"
+			>
+				<div
+					class="relative text-3xl before:absolute before:left-[50%] before:top-[60%] before:-z-20 before:w-screen before:-translate-x-1/2 before:border-t-[1px] before:border-zinc-700/40 max-sm:text-2xl"
+				>
+					Ruth Jaquette
+				</div>
+				<div class="pl-8 font-dosis text-2xl text-gray-300 max-sm:text-xl">rj30473@pausd.us</div>
+			</div>
+			<div
+				class="relative px-16 py-6 before:absolute before:-left-40 before:top-9 before:h-4 before:w-32 before:border-l-[1px] before:border-t-[1px] before:border-gray-600 after:absolute after:-bottom-28 after:-right-7 after:h-[36rem] after:w-24 after:border-b-[1px] after:border-r-[1px] after:border-zinc-600 max-sm:px-0"
+			>
+				<div
+					class="relative text-3xl before:absolute before:left-[50%] before:top-[60%] before:-z-20 before:w-screen before:-translate-x-1/2 before:border-t-[1px] before:border-zinc-700/40 max-sm:text-2xl"
+				>
+					Dylan Lu
+				</div>
+				<div class="pl-8 font-dosis text-2xl text-gray-300 max-sm:text-xl">dl30486@pausd.us</div>
+			</div>
+			<div
+				class="relative px-16 py-6 before:absolute before:-left-40 before:top-9 before:h-4 before:w-32 before:border-l-[1px] before:border-t-[1px] before:border-gray-600 after:absolute after:-right-44 after:bottom-3.5 after:h-12 after:w-64 after:border-r-[1px] after:border-t-[1px] after:border-zinc-600 max-sm:px-0"
+			>
+				<div
+					class="relative text-3xl before:absolute before:left-[50%] before:top-[60%] before:-z-20 before:w-screen before:-translate-x-1/2 before:border-t-[1px] before:border-zinc-700/40 max-sm:text-2xl"
+				>
+					Mrs. Hall
+				</div>
+				<div class="pl-8 font-dosis text-2xl text-gray-300 max-sm:text-xl">lhall@pausd.org</div>
+			</div>
+		</div>
 	</div>
 </Container>
 <Footer />
